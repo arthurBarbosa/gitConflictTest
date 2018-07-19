1555d0f35b596c067c42d9ee4b1030a38ec3ad74
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBAM.ORG.BR>
2018-07-19 15:39:18 -0400

    conflito resolvido

--- conflitos.js
+++ conflitos.js
@@ -5,13 +5,12 @@ var aux = 0;
 var subtracao = 1;
 
 var mult = x;
-<<<<<<< HEAD
-=======
+
 
 function subtrair(x, y){
     return x-y;
 }
->>>>>>> 1a6d2cca798b567947b8a98f722a34efae0d591c
+
 
 function contagem_tempo(segundos){
     var teste = 0;
@@ -27,9 +26,6 @@ function contagem_tempo(segundos){
 function multiplicar(x, y){
 	return x * y;
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 1a6d2cca798b567947b8a98f722a34efae0d591c
 
 
